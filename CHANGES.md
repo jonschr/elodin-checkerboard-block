004f71eb639d1a3992f68f7f52851a95101879c7
jonschr <jonschr@gmail.com>
2020-08-31 16:14:11 -0500

    Adding the update checker

---
+++ CHANGES.md
@@ -0,0 +1,12 @@
+### 1.0
+* Adding fade effect
+* Adding background color
+* Adding the image setting
+* Adding parallax effect
+* Adding 
+
+### 0.1
+* Adding build process for sass
+* Adding the basic block functionality
+* Adding changes.md
+* Adding a basic stylesheet with no options
--- elodin-checkerboard-block.php
+++ elodin-checkerboard-block.php
@@ -3,7 +3,7 @@
 	Plugin Name: Checkerboard Block
 	Plugin URI: https://elod.in
     Description: Just another checkerboard block
-	Version: 0.1
+	Version: 1.0
     Author: Jon Schroeder
     Author URI: https://elod.in
 
@@ -28,7 +28,7 @@ if ( !defined( 'ABSPATH' ) ) {
 define( 'CHECKERBOARD_BLOCK', dirname( __FILE__ ) );
 
 // Define the version of the plugin
-define ( 'CHECKERBOARD_BLOCK_VERSION', '0.1' );
+define ( 'CHECKERBOARD_BLOCK_VERSION', '1.0' );
 
 
 add_action('acf/init', 'checkerboard_register_block');
@@ -58,35 +58,100 @@ function checkerboard_register_block() {
     }
 }
 
+
 function checkerboard_render( $block, $content = '', $is_preview = false, $post_id = 0 ) {
     
+    //* Default class
+    $className = 'checkerboard';
+    
+    //* Default ID
+    $id = 'checkerboard-' . $block['id'];
+    
     //* Get settings
+    $alignment = get_field( 'alignment' );
+    $background_image = get_field( 'background_image' );
+    $background_attachment = get_field( 'background_attachment' );
+    $section_background_color = get_field( 'section_background_color' );
+    $minimum_height = get_field( 'minimum_height' );
+    $minimum_height_mobile = get_field( 'minimum_height_mobile' );
+    $add_fade_effect = get_field( 'add_fade_effect' );
+    
     // Create id attribute allowing for custom "anchor" value.
-    $id = 'testimonial-' . $block['id'];
-    if( !empty($block['anchor']) ) {
+    if( !empty($block['anchor']) ) 
         $id = $block['anchor'];
-    }
 
     // Create class attribute allowing for custom "className" and "align" values.
-    $className = 'testimonial';
-    if( !empty($block['className']) ) {
+    if( !empty($block['className']) )
         $className .= ' ' . $block['className'];
-    }
-    if( !empty($block['align']) ) {
-        $className .= ' align' . $block['align'];
-    }
 
-    // Load values and assing defaults.
-    $text = get_field('testimonial') ?: 'Your testimonial here...';
-    $author = get_field('author') ?: 'Author name';
-    $role = get_field('role') ?: 'Author role';
-    $image = get_field('image') ?: 295;
-    $background_color = get_field('background_color');
-    $text_color = get_field('text_color');
+    if( !empty($block['align']) )
+        $className .= ' align' . $block['align'];
+    
+    // Alignment class (this is a setting, not the default align)
+    if ( $alignment == true )
+        $className .= ' ' . 'checkerboard-image-right';
+        
+    if ( $background_attachment == true )
+        $className .= ' ' . 'background-fixed';
+        
+        
+    ?>
+    <style>
+        
+        @media( min-width: 960px ) { 
+            #checkerboard-<?php echo $block['id']; ?> .checkerboard-image {
+                <?php 
+                if ( $minimum_height ) {
+                    printf( 'min-height: %spx !important;', $minimum_height );
+                }
+                ?>
+            }
+        }
+        
+        @media( min-width: 600px and max-width: 960px ) { 
+            #checkerboard-<?php echo $block['id']; ?> .checkerboard-image {
+                <?php 
+                if ( $minimum_height_tablet ) {
+                    printf( 'min-height: %spx !important;', $minimum_height_tablet );
+                }
+                ?>
+            }
+        }
+        
+        @media( max-width: 600px ) { 
+            #checkerboard-<?php echo $block['id']; ?> .checkerboard-image {
+                <?php 
+                if ( $minimum_height_mobile ) {
+                    printf( 'min-height: %spx !important;', $minimum_height_mobile );
+                }
+                ?>
+            }
+        }
+        
+        @media( min-width: 600px ) { 
+            #checkerboard-<?php echo $block['id']; ?> .checkerboard-image .fade {
+                <?php 
+                if ( $section_background_color && $add_fade_effect && !$alignment ) {
+                    printf( 'background: linear-gradient( 90deg, transparent, %s ) !important;', $section_background_color );
+                }
+                
+                if ( $section_background_color && $add_fade_effect && $alignment ) {
+                    printf( 'background: linear-gradient( 90deg, %s, transparent ) !important;', $section_background_color );
+                }
+                ?>
+            }
+        }
+        
+    </style>
+    <?php
     
     //* Render
-    printf( '<div id="%s" class="checkerboard-wrap %s">', $id, $className );
-        echo '<div class="checkerboard-image">';
+    printf( '<div id="%s" class="checkerboard-wrap %s" style="background-color:%s">', $id, $className, $section_background_color );
+        printf( '<div class="checkerboard-image" style="background-image:url(%s);">', $background_image );
+        
+            if ( $add_fade_effect == true )
+                printf( '<div class="fade"></div>');
+        
         echo '</div>';
         echo '<div class="checkerboard-content">';
             echo '<div class="checkerboard-content-wrap">';
@@ -99,3 +164,62 @@ function checkerboard_render( $block, $content = '', $is_preview = false, $post_
 function checkerboard_enqueue() {
     wp_enqueue_style( 'checkerboard-block-style', plugin_dir_url( __FILE__ ) . 'css/checkerboard.css', array(), CHECKERBOARD_BLOCK_VERSION, 'screen' );
 }
+
+function checkerboard_get_the_colors() {
+	
+	// get the colors
+    $color_palette = current( (array) get_theme_support( 'editor-color-palette' ) );
+
+	// bail if there aren't any colors found
+	if ( !$color_palette )
+		return;
+
+	// output begins
+	ob_start();
+
+	// output the names in a string
+	echo '[';
+		foreach ( $color_palette as $color ) {
+			echo "'" . $color['color'] . "', ";
+		}
+	echo ']';
+    
+    return ob_get_clean();
+
+}
+
+add_action( 'acf/input/admin_footer', 'checkerboard_register_acf_color_palette' );
+function checkerboard_register_acf_color_palette() {
+
+    $color_palette = checkerboard_get_the_colors();
+    if ( !$color_palette )
+        return;
+    
+    ?>
+    <script type="text/javascript">
+        (function( $ ) {
+            acf.add_filter( 'color_picker_args', function( args, $field ){
+
+                // add the hexadecimal codes here for the colors you want to appear as swatches
+                args.palettes = <?php echo $color_palette; ?>
+
+                // return colors
+                return args;
+
+            });
+        })(jQuery);
+    </script>
+    <?php
+
+}
+
+// Updater
+require 'vendor/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/jonschr/elodin-checkerboard-block',
+	__FILE__,
+	'elodin-checkerboard-block'
+);
+
+// Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
